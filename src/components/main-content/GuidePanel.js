bfce8a3b2250c3ed52e8fcd6d8220f1770377034
tnah-hcmus <tnah.work@gmail.com>
2020-06-10 10:39:57 +0700

    CSS for iframe (fit to content-board)

--- src/components/main-content/GuidePanel.js
+++ src/components/main-content/GuidePanel.js
@@ -6,7 +6,7 @@ const GuidePanel = (props) => (
         props.tabs.map((tab, index) =>  (
         <div>
             <div class="popup" id={tab.id} onClick = {(e) => e.target.parentElement.classList.remove('show-popup')}>
-                <iframe width="720" height="400" src={tab.link} frameborder="0" allowFullScreen></iframe>
+                <iframe width="80%" height="60%" src={tab.link} frameborder="0" allowFullScreen center></iframe>
             </div> 
         </div>
         ))
