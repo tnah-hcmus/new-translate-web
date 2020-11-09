641e93bf5280c79e4248cb25427f25e4e6702566
tnah-hcmus <tnah.work@gmail.com>
2020-11-09 17:46:28 +0700

    Store session

--- src/actions/auth/auth.js
+++ src/actions/auth/auth.js
@@ -7,7 +7,18 @@ export const login = (uid) => ({
 
 export const startLoginGoogle = () => {
   return () => {
-    return firebase.auth().signInWithRedirect(googleAuthProvider);
+    return firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL)
+          .then(function() {
+            // Existing and future Auth states are now persisted in the current
+            // session only. Closing the window would clear any existing state even
+            // if a user forgets to sign out.
+            // ...
+            // New sign-in will be persisted with session persistence.
+            return firebase.auth().signInWithRedirect(googleAuthProvider);
+          })
+          .catch(function(error) {
+            console.log(error);
+          }); 
   };
 };
 
--- src/store/store.js
+++ src/store/store.js
@@ -33,8 +33,8 @@ const localDB = localForage.createInstance({
 
 const persistConfig = {
   key: 'root',
-  whitelist: ['auth', 'theme', 'replies'],
-  blacklist: ['tabs', 'credit', 'category'],
+  whitelist: ['theme', 'replies'],
+  blacklist: ['tabs', 'credit', 'category', 'auth'],
   storage: localDB,
 }
 
